92eb7f37a6df7e952ddfe3261dfc3cf58db55b2f
Aung Myo Aye <aungmyoaye101@gmail.com>
2026-01-12 22:37:27 +0630

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { HeroUIProvider } from "@heroui/system";
+import { Providers } from "./provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+
       </body>
-    </html>
-  );
+    </html >
+  )
 }
