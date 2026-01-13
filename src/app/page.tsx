e8e579ec5d6a6d804c347330d9ccbf4debc0647d
Aung Myo Aye <aungmyoaye101@gmail.com>
2026-01-13 23:08:30 +0630

    feat : add card list,

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 
+import HotelCardList from "@/components/home/card";
 import Hero from "@/components/home/hero";
 
 export default function Home() {
   return (
     <section>
       <Hero />
+      <HotelCardList />
     </section>
   );
 }
