8de2e3794b4004bc7d27b7d2ea6c26ccf1b6df87
Aung Myo Aye <aungmyoaye101@gmail.com>
2026-01-18 21:17:54 +0630

    feat : add react hook form and zod schema for authentication.

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,11 +1,24 @@
 
 'use client';
+import { loginSchema, type loginType } from '@/validations/auth-schema';
 import { Button, Card, CardBody, CardHeader, Form, Input } from '@heroui/react'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Eye, EyeClosed, EyeOff, Lock, Mail } from 'lucide-react';
 import Image from 'next/image'
-import React from 'react'
+import Link from 'next/link';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
-    const [action, setAction] = React.useState();
+    const [isVisible, setIsVisible] = useState<boolean>(false)
+    const toggleVisible = () => setIsVisible(pre => !pre)
+    const { register, handleSubmit, formState: { errors, isLoading } } = useForm<loginType>({
+        resolver: zodResolver(loginSchema)
+    })
+
+    const onSubmit = async (data: loginType) => {
+        console.log(data)
+    }
     return (
         <section className='h-screen flex justify-end items-center'>
             <Image
@@ -16,55 +29,60 @@ const Login = () => {
                 className='object-cover absolute inset-0 -z-10'
 
             />
-            <Card className='min-w-xs max-w-4xl px-4 py-6 '>
+            <Card className=' px-4 py-6 min-w-xs w-full max-w-100 bg-background/90'>
                 <CardHeader className='flex flex-col gap-1'>
                     <h1 className='text-2xl font-semibold text-center'>Login</h1>
                     <p>Please login your account.</p>
                 </CardHeader>
                 <CardBody>
-                    <Form
+                    <form
                         className="w-full max-w-xs flex flex-col gap-4"
-                        // onReset={() => setAction("reset")}
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            let data = Object.fromEntries(new FormData(e.currentTarget));
-
-                            // setAction(`submit ${JSON.stringify(data)}`);
-                        }}
+                        onSubmit={handleSubmit(onSubmit)}
                     >
                         <Input
-                            isRequired
-                            errorMessage="Please enter a valid username"
-                            label="Username"
+                            {
+                            ...register('email')
+                            }
+                            isInvalid={!!errors.email}
+                            errorMessage={errors.email?.message}
+                            label="Email"
                             labelPlacement="outside"
-                            name="username"
-                            placeholder="Enter your username"
-                            type="text"
+                            placeholder="Enter your email"
+                            type="email"
+                            startContent={<Mail size={20} className='text-default-400' />}
                         />
 
                         <Input
-                            isRequired
-                            errorMessage="Please enter a valid email"
-                            label="Email"
+                            {
+                            ...register('password')
+                            }
+
+                            isInvalid={!!errors.password}
+                            errorMessage={errors.password?.message}
+                            label="Password"
                             labelPlacement="outside"
-                            name="email"
-                            placeholder="Enter your email"
-                            type="email"
+                            placeholder="Enter your password"
+                            type={isVisible ? 'text' : "password"}
+                            startContent={<Lock size={20} className='text-default-400' />}
+                            endContent={
+
+                                <Button
+
+                                    isIconOnly size='sm'
+
+                                    onPress={() => toggleVisible()}>
+                                    {isVisible ? <Eye /> : <EyeOff />}</Button>
+
+                            }
                         />
-                        <div className="flex gap-2">
-                            <Button color="primary" type="submit">
-                                Submit
-                            </Button>
-                            <Button type="reset" variant="flat">
-                                Reset
-                            </Button>
-                        </div>
-                        {action && (
-                            <div className="text-small text-default-500">
-                                Action: <code>{action}</code>
-                            </div>
-                        )}
-                    </Form>
+
+                        <Button color="primary" type="submit">
+                            Submit
+                        </Button>
+
+
+                    </form>
+                    <p className='text-sm my-2'>Don't  have an account ? <Link href={'/signup'} className='text-violet-800'>Signup</Link></p>
                 </CardBody>
             </Card>
         </section>
--- src/components/auth/signup.tsx
+++ src/components/auth/signup.tsx
@@ -1,11 +1,25 @@
 
 'use client';
+import { registerSchema, registerType } from '@/validations/auth-schema';
 import { Button, Card, CardBody, CardHeader, Form, Input } from '@heroui/react'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Eye, EyeOff, Lock, Mail } from 'lucide-react';
 import Image from 'next/image'
-import React from 'react'
+import Link from 'next/link';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
 
 const Signup = () => {
-    const [action, setAction] = React.useState();
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+    const toggleVisible = () => setIsVisible(pre => !pre);
+
+    const { register, handleSubmit, formState: { errors, isLoading } } = useForm<registerType>({
+        resolver: zodResolver(registerSchema)
+    })
+
+    const onSubmit = async (data: registerType) => {
+        console.log(data)
+    }
     return (
         <section className='h-screen flex justify-end items-center'>
             <Image
@@ -16,65 +30,76 @@ const Signup = () => {
                 className='object-cover absolute inset-0 -z-10'
 
             />
-            <Card >
+            <Card className='min-w-xs w-full max-w-100 bg-background/90'>
                 <CardHeader className='flex flex-col gap-1'>
                     <h1 className='text-2xl font-semibold text-center'>Signup</h1>
                     <p>Please create new account.</p>
                 </CardHeader>
-                <CardBody className='min-w-xs max-w-4xl px-4 py-6 '>
-                    <Form
-                        className="w-full max-w-xs flex flex-col gap-4"
-                        // onReset={() => setAction("reset")}
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            let data = Object.fromEntries(new FormData(e.currentTarget));
-
-                            // setAction(`submit ${JSON.stringify(data)}`);
-                        }}
+                <CardBody className=' px-4 py-6 '>
+                    <form
+                        className=" flex flex-col gap-4"
+                        onSubmit={handleSubmit(onSubmit)}
                     >
                         <Input
-                            isRequired
-                            errorMessage="Please enter a valid username"
-                            label="Username"
+                            {
+                            ...register('name')
+                            }
+                            isInvalid={!!errors.name}
+                            errorMessage={errors.name?.message}
+                            label="Name"
                             labelPlacement="outside"
-                            name="username"
-                            placeholder="Enter your username"
+                            placeholder="Enter your name"
                             type="text"
                         />
 
 
                         <Input
-                            isRequired
-                            errorMessage="Please enter a valid email"
+                            {
+                            ...register('email')
+                            }
+                            isInvalid={!!errors.email}
+                            errorMessage={errors.email?.message}
                             label="Email"
                             labelPlacement="outside"
-                            name="email"
                             placeholder="Enter your email"
                             type="email"
+                            startContent={<Mail size={20} className='text-default-400' />}
                         />
+
                         <Input
-                            isRequired
-                            errorMessage="Please enter a valid password"
+                            {
+                            ...register('password')
+                            }
+
+                            isInvalid={!!errors.password}
+                            errorMessage={errors.password?.message}
                             label="Password"
                             labelPlacement="outside"
-                            name="password"
                             placeholder="Enter your password"
-                            type="password"
+                            type={isVisible ? 'text' : "password"}
+                            startContent={<Lock size={20} className='text-default-400' />}
+                            endContent={
+
+                                <Button
+
+                                    isIconOnly size='sm'
+
+                                    onPress={() => toggleVisible()}>
+                                    {isVisible ? <Eye /> : <EyeOff />}</Button>
+
+                            }
                         />
-                        <div className="flex gap-2">
-                            <Button color="primary" type="submit">
-                                Submit
-                            </Button>
-                            <Button type="reset" variant="flat">
-                                Reset
-                            </Button>
-                        </div>
-                        {action && (
-                            <div className="text-small text-default-500">
-                                Action: <code>{action}</code>
-                            </div>
-                        )}
-                    </Form>
+
+
+                        <Button
+                            isLoading={isLoading}
+                            color="primary"
+                            type="submit">
+                            Submit
+                        </Button>
+
+                    </form>
+                    <p className='text-sm my-2'>Already have an account ? <Link href={'/login'} className='text-violet-800'>Login</Link></p>
                 </CardBody>
             </Card>
         </section>
