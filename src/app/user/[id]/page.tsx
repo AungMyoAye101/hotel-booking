66f4c55c8d1fe2cb7416430e25c432255751f1c6
Aung Myo Aye <aungmyoaye101@gmail.com>
2026-01-21 22:02:47 +0630

    feat: enhance axios API client with token refresh logic and error handling

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,8 +1,6 @@
 
 import Profile from "@/components/user/profile";
-import { serverFetch } from "@/hooks/api";
-import { APIResponse } from "@/types";
-import { User } from "@/types/user-type";
+
 
 
 
@@ -13,7 +11,6 @@ type Params = {
 const page = async ({ params }: { params: Promise<Params> }) => {
     const id = (await params).id;
 
-    const data = await serverFetch<APIResponse<{ user: User }>>(`/users/${id}`);
 
     return (
         <div className="min-h-screen px-4 py-20 max-w-4xl mx-auto">
--- src/hooks/axios-api.ts
+++ src/hooks/axios-api.ts
@@ -2,7 +2,8 @@
 import { BASE_URL } from "@/lib";
 import { useAuth } from "@/stores/auth-store";
 import { APIResponse } from "@/types";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+
 
 
 
@@ -13,7 +14,7 @@ export const apiClient = axios.create({
 
 apiClient.interceptors.request.use(config => {
     const token = useAuth.getState().token;
-    console.log(token)
+
     if (token) {
         config.headers.Authorization = `Bearer ${token}`
     }
@@ -23,12 +24,50 @@ apiClient.interceptors.request.use(config => {
         return Promise.reject(error)
     })
 
+let isRefeshing = false;
+
+type failedQueue = {
+    resolve: (token: string) => void,
+    reject: (error: AxiosError) => void
+}
+let failedQueue: failedQueue[] = [];
+
+const processQueue = (error: AxiosError | null, token?: string) => {
+    failedQueue.forEach(({ resolve, reject }) => {
+        if (error) {
+            reject(error)
+        } else {
+            resolve(token ?? "")
+        }
+    })
+    failedQueue = [];
+}
+
 apiClient.interceptors.response.use(
     (response) => response,
     async (error) => {
+        const clearAuth = useAuth.getState().reset;
         const original_request = error.config;
         if (error.response.status === 401 && !original_request._retry) {
+            if (
+                original_request.url.includes('/login') ||
+                original_request.url.includes('/signup') ||
+                original_request.url.includes('/refresh')
+
+            ) {
+                return Promise.reject(error);
+            }
             original_request._retry = true;
+
+            //refreshing 
+            if (isRefeshing) {
+                return new Promise((resolve, reject) => {
+                    failedQueue.push({ resolve, reject })
+                }).then((token) => {
+                    original_request.headers.Authorization = `Bearer ${token}`
+                })
+            }
+            isRefeshing = true;
             try {
 
                 const { data } = await axios.post<APIResponse<{ token: string }>>(`${BASE_URL}/auth/refresh`, "", {
@@ -36,14 +75,21 @@ apiClient.interceptors.response.use(
                 })
                 const token = data.result.token;
 
+                processQueue(null, token)
                 useAuth.getState().setToken(token);
 
                 original_request.headers.Authorization = `Bearer ${token}`;
                 return apiClient(original_request)
 
             } catch (refreshError) {
-                console.warn(refreshError)
+
+                processQueue(refreshError as AxiosError);
+                failedQueue = [];
+                clearAuth();
+                isRefeshing = false;
                 return Promise.reject(refreshError)
+            } finally {
+                isRefeshing = false;
             }
         }
 
