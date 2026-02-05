9bce47f127ccba87992f9a71f202b39f634970e5
Aung Myo Aye <aungmyoaye101@gmail.com>
2026-02-05 23:37:34 +0630

    refactor : payment page.

--- src/app/booking/[id]/payment/page.tsx
+++ src/app/booking/[id]/payment/page.tsx
@@ -1,9 +1,10 @@
+import PaymentDetailsForm from '@/components/booking/payment-details'
 import React from 'react'
 
 const page = () => {
-    return (
-        <div>page</div>
-    )
+    return <PaymentDetailsForm
+    //  bookingId={booking?._id} userId={booking?.user._id} amount={booking?.totalPrice} 
+    />
 }
 
 export default page
--- src/components/booking/payment-details.tsx
+++ src/components/booking/payment-details.tsx
@@ -3,7 +3,7 @@
 import { useCreatePayment } from '@/hooks/use-payment';
 
 import { PaymentMethodType } from '@/types';
-import { Button, Card, CardBody, DatePicker, Input } from '@heroui/react'
+import { Button, Card, CardBody, CardHeader, DatePicker, Input } from '@heroui/react'
 
 import Image from 'next/image';
 
@@ -15,16 +15,16 @@ type Props = {
     amount: number
 }
 
-const PaymentDetailsForm = ({ bookingId, userId, amount }: Props) => {
+const PaymentDetailsForm = () => {
 
 
     const { mutate, isPending } = useCreatePayment()
     const handleSubmit = () => {
         mutate({
-            bookingId,
-            userId,
-            paymentMethod: payment,
-            amount,
+            // bookingId,
+            // userId,
+            // paymentMethod: payment,
+            // amount,
         },
         )
 
@@ -33,91 +33,94 @@ const PaymentDetailsForm = ({ bookingId, userId, amount }: Props) => {
     const [payment, setPayment] = useState<PaymentMethodType>('CARD')
 
     return (
-        <Card>
+        <Card className='p-4'>
+            <CardHeader>
+                <div>
+                    <h1 className='head-1 mb-2'>Choose Payment Method </h1>
+                    <p className='text-sm'>Select a payment option to proceed</p>
+                </div>
+            </CardHeader>
             <CardBody>
-                <div className='  p-4 space-y-4'>
-                    <div>
-                        <h1 className='head-1 mb-2'>Choose Payment Method </h1>
-                        <p className='text-sm'>Select a payment option to proceed</p>
-                    </div>
-                    {/* -----------Payment option------------ */}
-                    <div className='flex flex-wrap gap-4 items-center justify-center'>
-                        <label htmlFor='card'>
-                            <Card className={`${payment === "CARD" ? 'bg-success-50 border-2 border-success-400' : 'bg-deafult'} w-40`} >
-                                <CardBody >
-                                    <div className={`flex flex-col items-center gap-2 `} >
 
 
-                                        <Image
-                                            src={'/card.png'}
-                                            alt='card icon'
-                                            width={80}
-                                            height={80}
+                {/* -----------Payment option------------ */}
+                <div className='flex flex-wrap gap-4 items-center justify-center'>
+                    <label htmlFor='card'>
+                        <Card className={`${payment === "CARD" ? 'bg-success-50 border-2 border-success-400' : 'bg-deafult'} w-44 h-36 flex items-center justify-center`} >
+                            <CardBody >
+                                <div className={`flex flex-col items-center gap-2 `} >
 
 
-                                        />
-                                        <div className='text-center'>
-                                            <h1 className=' font-medium'>Card / debait card</h1>
-                                            <p className='text-sm'>Pay with card</p>
-                                        </div>
+                                    <Image
+                                        src={'/card.png'}
+                                        alt='card icon'
+                                        width={80}
+                                        height={80}
+
+
+                                    />
+                                    <div className='text-center'>
+                                        <h1 className=' font-medium'>Card / debait card</h1>
+                                        <p className='text-sm'>Pay with card</p>
                                     </div>
-                                </CardBody>
-                            </Card>
+                                </div>
+                            </CardBody>
+                        </Card>
 
 
-                            <input type="radio" checked={payment === "CARD"} name='payment' id='card' onChange={() => setPayment("CARD")} className='hidden' />
-                        </label>
-                        <label htmlFor='mobile'>
+                        <input type="radio" checked={payment === "CARD"} name='payment' id='card' onChange={() => setPayment("CARD")} className='hidden' />
+                    </label>
+                    <label htmlFor='mobile'>
 
-                            <Card className={`${payment === 'MOBILE_BANKING' ? 'bg-success-50 border-2 border-success-400' : 'bg-deafult'} w-40`} >
-                                <CardBody >
-                                    <div className={`flex flex-col items-center gap-2 `} >
-
-
-                                        <Image
-                                            src={'/mobile-banking.svg'}
-                                            alt='mobile banking icon'
-                                            width={60}
-                                            height={60}
+                        <Card className={`${payment === 'MOBILE_BANKING' ? 'bg-success-50 border-2 border-success-400' : 'bg-deafult'} w-44 h-36 flex items-center justify-center`} >
+                            <CardBody >
+                                <div className={`flex flex-col items-center gap-2 `} >
 
 
-                                        />
-                                        <div className='text-center'>
-                                            <h1 className='text-lg font-medium'>Mobile Banking</h1>
-                                            <p className='text-sm'>Pay via e-wallet</p>
-                                        </div>
+                                    <Image
+                                        src={'/mobile-banking.svg'}
+                                        alt='mobile banking icon'
+                                        width={60}
+                                        height={60}
+
+
+                                    />
+                                    <div className='text-center'>
+                                        <h1 className='text-lg font-medium'>Mobile Banking</h1>
+                                        <p className='text-sm'>Pay via e-wallet</p>
                                     </div>
-                                </CardBody>
-                            </Card>
-                            <input type="radio" checked={payment === "MOBILE_BANKING"} name='payment' id='mobile' onChange={() => setPayment("MOBILE_BANKING")} className='hidden' />
-                        </label>
+                                </div>
+                            </CardBody>
+                        </Card>
+                        <input type="radio" checked={payment === "MOBILE_BANKING"} name='payment' id='mobile' onChange={() => setPayment("MOBILE_BANKING")} className='hidden' />
+                    </label>
 
-                        <label htmlFor='bank'>
-
-
-                            <Card className={`${payment === 'BANK' ? 'bg-success-50 border-2 border-success-400' : 'bg-deafult'} w-40`} >
-                                <CardBody >
-                                    <div className={`flex flex-col items-center gap-2 `} >
-                                        <Image
-                                            src={'/bank.svg'}
-                                            alt='bank icon'
-                                            width={60}
-                                            height={60}
+                    <label htmlFor='bank'>
 
 
-                                        />
-                                        <div className='text-center'>
-                                            <h1 className='text-lg font-medium'>Bank Transfer</h1>
-                                            <p className='text-sm'>Pay via bank</p>
-                                        </div>
+                        <Card className={`${payment === 'BANK' ? 'bg-success-50 border-2 border-success-400' : 'bg-deafult'}  w-44 h-36 flex items-center justify-center`} >
+                            <CardBody >
+                                <div className={`flex flex-col items-center gap-2 `} >
+                                    <Image
+                                        src={'/bank.svg'}
+                                        alt='bank icon'
+                                        width={60}
+                                        height={60}
+
+
+                                    />
+                                    <div className='text-center'>
+                                        <h1 className='text-lg font-medium'>Bank Transfer</h1>
+                                        <p className='text-sm'>Pay via bank</p>
                                     </div>
-                                </CardBody>
-                            </Card>
-                            <input type="radio" checked={payment === 'BANK'} name='payment' id='bank' onChange={() => setPayment("BANK")} className='hidden' />
+                                </div>
+                            </CardBody>
+                        </Card>
+                        <input type="radio" checked={payment === 'BANK'} name='payment' id='bank' onChange={() => setPayment("BANK")} className='hidden' />
 
-                        </label>
-                    </div>
-                    {/* {
+                    </label>
+                </div>
+                {/* {
                         payment === "CARD" &&
                         <div className='py-4 space-y-4'>
                             <Input type='text' placeholder='123 456 789' label="Card number" labelPlacement='outside' radius='sm' />
@@ -136,15 +139,15 @@ const PaymentDetailsForm = ({ bookingId, userId, amount }: Props) => {
                     } */}
 
 
-                    <Button
-                        isLoading={isPending}
-                        variant='solid'
-                        color='primary'
-                        radius='sm'
-                        fullWidth
-                        onPress={() => handleSubmit()}
-                    >Proceed to Payment</Button>
-                </div>
+                <Button
+                    isLoading={isPending}
+                    variant='solid'
+                    color='primary'
+                    radius='sm'
+                    fullWidth
+                    onPress={() => handleSubmit()}
+                >Proceed to Payment</Button>
+
             </CardBody>
         </Card>
     )
