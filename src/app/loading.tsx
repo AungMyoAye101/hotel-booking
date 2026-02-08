706fa7b5ee8b1d80840af885f45e329d3d7bf728
Aung Myo Aye <aungmyoaye101@gmail.com>
2026-02-08 13:25:55 +0630

    feat : add loading , error and not found page,

---
+++ src/app/loading.tsx
@@ -0,0 +1,14 @@
+
+import LoadingSpinner from "@/components/ui/spinner";
+
+
+
+const Loading = () => {
+    return (
+        <section className="h-screen bg-white/60 flex justify-center items-center">
+            <LoadingSpinner />
+        </section>
+    )
+}
+
+export default Loading
