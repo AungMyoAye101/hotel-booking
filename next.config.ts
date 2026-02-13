5f8b49dc34a18acb69b1c6d8990c80e6e9c4515a
Aung Myo Aye <aungmyoaye101@gmail.com>
2026-02-13 21:05:31 +0630

    fix : image to unoptimized.

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com'
       }
-    ]
+    ],
+
   },
 }
 
